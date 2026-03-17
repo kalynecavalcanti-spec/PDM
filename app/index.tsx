0c2d49aa354e3c5863f6251f43fc612626beadd6
Kalyne <kalyne.cavalcanti@academico.ifpb.edu.br>
2026-03-17 11:20:53 -0300

    Teste TextInput

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {TextInput, View, Text } from "react-native";
 
 export default function Index() {
+  const [nome, setNome]= useState('');
   return (
     <View
       style={{
         flex: 1,
+        padding: 16,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Kalyne</Text>
+ <TextInput
+ placeholder="Digite Algo..."
+ onChangeText={ novoTexto => setNome(novoTexto) }
+ value={nome}
+ style={{
+ width: "100%",
+ padding: 5,
+ borderWidth: 1
+}}
+ />
+ <Text>{nome}</Text>
     </View>
   );
 }
