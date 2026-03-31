470fc7f9e984d916b3fabed80c48896ffa05fcf1
Kalyne <kalyne.cavalcanti@academico.ifpb.edu.br>
2026-03-31 11:26:25 -0300

    modifiquei o layout e index, e add util

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,77 @@
 import { useState } from "react";
-import {TextInput, View, Text } from "react-native";
+import { Button, FlatList, Text, TextInput, View } from "react-native";
+import { nextId } from "../util/geral";
 
 export default function Index() {
-  const [nome, setNome]= useState('');
+  const [nome, setNome] = useState('');
+  const [lista, setLista] = useState([]);
+
+  function adiciona(){
+    setLista([...lista, {nome: nome}]);
+  }
+
+  function remove(nome) {
+    setLista( lista.filter( item => item.nome !== nome ) );
+  }
+
+  function RenderItem({contato}) {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: '#dddddd',
+          width: '100%',
+          padding: 5,
+          alignItems: 'center'
+        }}
+      >
+        <Text style={{flex: 1}}>{contato.nome}</Text>
+        <Button
+          title="-"
+          onPress={ () => remove(contato.nome) }
+        />
+      </View>
+    )
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        padding: 16,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        backgroundColor: 'white'
       }}
     >
- <TextInput
- placeholder="Digite Algo..."
- onChangeText={ novoTexto => setNome(novoTexto) }
- value={nome}
- style={{
- width: "100%",
- padding: 5,
- borderWidth: 1
-}}
- />
- <Text>{nome}</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%'
+        }}
+      >
+        <TextInput
+          placeholder="Digite um nome ..."
+          value={nome}
+          onChangeText={ t => setNome(t)}
+          style={{
+            flex: 1,
+            borderWidth: 1,
+            padding: 5
+          }}
+        />
+        <Button 
+          title="+"
+          onPress={ adiciona }
+        />
+      </View>
+      <FlatList
+        data={lista}
+        renderItem={ ({item}) => <RenderItem contato={item} /> }
+        ItemSeparatorComponent={ () => <View style={{ height: 2 }} />}
+        style={{
+          width: '100%'
+        }}
+      />
     </View>
   );
 }
